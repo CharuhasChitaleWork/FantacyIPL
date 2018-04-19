ced6c3ea1bad2798d526d8391cdb876f1064552b
Charuhas Chitale <charuhas.chitale05@gmail.com>
2018-04-19 19:27:46 +0530

    image added

--- app/src/main/java/com/fipl/charuhaschitale/fantacy_ipl/HomeActivity.java
+++ app/src/main/java/com/fipl/charuhaschitale/fantacy_ipl/HomeActivity.java
@@ -3,15 +3,20 @@ package com.fipl.charuhaschitale.fantacy_ipl;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class HomeActivity extends AppCompatActivity {
 Button creategroupe,joinGroupe;
+TextView text;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
         creategroupe = (Button) findViewById(R.id.buttonCreateGroupe);
         joinGroupe = (Button) findViewById(R.id.buttonJoinGroupe);
+        //text =(TextView) findViewById(R.id.TempText);
+        //text.setText("YOLO");
 
     }
 }
