dd8c6950bc92f707a67cb5d3c8abd42fb06fef01
Charuhas Chitale <charuhas.chitale05@gmail.com>
2018-04-20 20:01:17 +0530

    live matches updated

--- app/src/main/java/com/fipl/charuhaschitale/fantacy_ipl/HomeActivity.java
+++ app/src/main/java/com/fipl/charuhaschitale/fantacy_ipl/HomeActivity.java
@@ -19,28 +19,32 @@ public class HomeActivity extends AppCompatActivity {
 
 TextView team11,team12,team21,team22;
 
-    private DatabaseReference mDatabase,ref;
+    private DatabaseReference mDatabase,ref,ref2,ref3,ref4,ref5,ref6;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-        creategroupe = (Button) findViewById(R.id.buttonCreateGroupe);
-        joinGroupe = (Button) findViewById(R.id.buttonJoinGroupe);
+
         team11 =(TextView) findViewById(R.id.Team11);
         team12 =(TextView) findViewById(R.id.Team12);
         team21 =(TextView) findViewById(R.id.Team21);
         team22 =(TextView) findViewById(R.id.Team22);
 
         mDatabase = FirebaseDatabase.getInstance().getReference();
-      ref = mDatabase.child("realtime_fixtures");
-  ref.addValueEventListener(new ValueEventListener() {
+      ref = mDatabase.child("realtime_fixtures").child("match1");
+        ref3 = ref.child("team1");
+        ref4 = ref.child("team2");
+      ref2 = mDatabase.child("realtime_fixtures").child("match2");
+            ref5 = ref.child("team1");
+            ref6 = ref.child("team2");
+  ref3.addValueEventListener(new ValueEventListener() {
       @Override
       public void onDataChange(DataSnapshot dataSnapshot) {
           String bun = dataSnapshot.getValue().toString();
           team11.setText(bun);
-        Object obj=  dataSnapshot.getValue();
-        team12.setText(obj.toString());
+
+
       }
 
       @Override
@@ -49,8 +53,49 @@ TextView team11,team12,team21,team22;
       }
   });
 
+        ref4.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                String bun = dataSnapshot.getValue().toString();
+                team12.setText(bun);
 
 
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+        ref5.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                String bun = dataSnapshot.getValue().toString();
+                team21.setText(bun);
+
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+        ref6.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                String bun = dataSnapshot.getValue().toString();
+                team22.setText(bun);
+
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
     }
 
 
